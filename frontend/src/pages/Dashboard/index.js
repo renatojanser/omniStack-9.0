9da193cdb47566954f98c554d9b4470018f11e6b
Renato Janser <rjanser@agenciabamboo.com.br>
2019-10-02 19:06:22 -0300

    Iniciei a tela de dashboard

---
+++ frontend/src/pages/Dashboard/index.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
+
+export default function Dashboard() {
+
+    const [spots, setSpots] = useState([]);
+
+    useEffect(() => {
+        async function loadSpots() {
+            const user_id = localStorage.getItem('user');
+            const response = await api.get('/dashboard', {
+                headers: { user_id }
+            });
+
+            setSpots(response.data);
+        }
+
+        loadSpots();
+    }, []);
+
+    return (
+        <div>
+            <ul className="spot-list">
+                {spots.map(spot => (
+                    <li key={spot._id}>
+                        <header />
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
