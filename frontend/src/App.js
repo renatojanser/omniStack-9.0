7909003d163765169c8fa1946e5440fd8272219c
Renato Janser <rjanser@agenciabamboo.com.br>
2019-10-02 16:53:44 -0300

    Ajuste na tela de login frontend e ajuste no backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <img src={logo} alt="AirCnc"/>
+function App() {
 
-        <div className="content">
-          <p>
-            Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-          </p>
+  const [email, setEmail] = useState('');
 
-          <form>
-            <label htmlFor="email">E-MAIL *</label>
-            <input type="email" id="email" placeholder="Seu melhor e-mail" />
-            <button className="btn" type="submit">Entrar</button>
-          </form>
-        </div>
-      </div>
-    );
+  async function handleSubmit(event){
+    event.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
   }
+
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnc"/>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+        </p>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+            id="email" 
+            type="email" 
+            placeholder="Seu melhor e-mail" 
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
+  );
+  
 }
 
 export default App;
