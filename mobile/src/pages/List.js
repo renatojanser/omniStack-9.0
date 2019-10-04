6187fbe21806d8e97279f823f7ac5250090752fa
Renato Janser <rjanser@agenciabamboo.com.br>
2019-10-04 11:06:36 -0300

    Finaliziei as telas de lists e de solicitar reserva

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,6 +1,40 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, AsyncStorage, Image, StyleSheet } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
-    return <View />
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storegedTechs => {
+            const techsArray = storegedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        });
+    }, []);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+            
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 40
+    },
+});
