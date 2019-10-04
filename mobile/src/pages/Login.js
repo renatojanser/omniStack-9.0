8e6e8430a9fc3a641578d4a348bee0aa710c1bd7
Renato Janser <rjanser@agenciabamboo.com.br>
2019-10-04 08:52:25 -0300

    Finalizei a tela de Login Mobile

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,18 +1,34 @@
-import React, { useState } from 'react';
-import { View, KeyboardAvoidingView, Platform, Image, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, AsyncStorage, KeyboardAvoidingView, Platform, Image, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
 import api from '../services/api';
 
 import logo from '../assets/logo.png';
 
-export default function Login() {
+export default function Login({ navigation }) {
 
     const [email, setEmail] = useState('');
     const [techs, setTechs] = useState('');
 
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if(user){
+                navigation.navigate('List');
+            }
+        });
+    }, []);
+
     async function handleSubmit() {
-        console.log(email);
-        console.log(techs);
+        const response = await api.post('/sessions', {
+            email
+        });
+
+        const { _id } = response.data;
+
+        await AsyncStorage.setItem('user', _id);
+        await AsyncStorage.setItem('techs', techs);
+
+        navigation.navigate('List');
     }
 
     return (
